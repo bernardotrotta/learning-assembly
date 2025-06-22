ac889a0cac27d1d3f94fcb44026b40fb6602526e
Bernardo Trotta <bernardotrotta@outlook.it>
2025-06-22 18:31:22 +0200

    Counter

--- test.asm
+++ test.asm
@@ -1,18 +1,32 @@
 CPU 8086
 SECTION data
-    A: DB 2
-    B: DB 4
+    A: DB 0
+    B: DB 9
 
 SECTION text
 ..start:
     MOV AX, data
     MOV DS, AX
+
     MOV AL, [A]
     MOV BL, [B]
-    MOV CL, AL
-    MOV AL, BL
-    MOV BL, CL
-
+    ; ADD AL, 01h     ; Replaced by INC
+    
+increment: 
+    INC AL
+    CALL print
+    CMP AL, BL
+    JNE increment
+    CALL fine
+    
 fine:
     MOV AX,4c00h
 	INT	21h
+
+print: 
+    ADD AL, '0'     ; To print the ASCII char
+    MOV DL, AL
+    MOV AH, 02h
+    INT 21h
+    SUB AL, '0'
+    RET
