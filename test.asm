03521d2cc6e36ac8c7665411770d72ad65152955
Bernardo Trotta <bernardotrotta@outlook.it>
2025-06-23 13:23:57 +0200

    Print byte array

--- test.asm
+++ test.asm
@@ -1,44 +1,50 @@
 CPU 8086
 SECTION data
-    A: DB 2
-    B: DB 3
-
+    A: DB 6, 4, 7, 8, 3, 9, 0, 5, 1, 2 ; 45
+    I: DW 10
 SECTION text
 ..start:
     MOV AX, data
     MOV DS, AX
-    MOV AL, 0
-    MOV CL, 0
-    ; ADD AL, 01h   ; Replaced by INC
-    
-sum: 
-    INC CL
-    ADD AL, [A]
-    CALL print
-    CMP CL, [B]      ; Replace BL with B?
-    JNE sum
+
+    MOV SI, 0
+    MOV AL, [A]
+    CALL sum_byte_array
     CALL fine
-    
+
 fine:
     MOV AX,4c00h
 	INT	21h
 
-print: 
-    PUSH AX         ; "AL" should be pushed to the stack. This will prevent it from being altered by INT 21h.
+sum_byte_array:
+    CALL print_number
+    CALL print_space
+    INC SI
+    ADD AL, [A+SI]
+    CMP SI, [I]
+    JNE sum_byte_array
+    RET
 
-    ADD AL, '0'     ; To print the ASCII char
+print_byte_array:
+    CALL print_number
+    CALL print_space
+    INC SI
+    MOV AL, [A+SI]
+    CMP SI, [I]
+    JNE print_byte_array
+    RET
 
-    MOV AH, 02h
-
+print_number:
+    PUSH AX
+    ADD AL, '0'
     MOV DL, AL
+    MOV AH, 02h 
     INT 21h
-
-    ; MOV DL, 2Ch     ; Comma
-    ; INT 21h
-
-    MOV DL, 20h     ; SP     
-    INT 21h
-
     POP AX
+    RET 
 
+print_space:
+    MOV DL, 20h
+    MOV AH, 02h 
+    INT 21h
     RET 
