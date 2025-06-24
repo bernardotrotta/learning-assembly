974e0aaf5d0ead7e46732ea3daaec6575c1ce569
Bernardo Trotta <bernardotrotta@outlook.it>
2025-06-24 19:58:59 +0200

    String reversal

--- test.asm
+++ test.asm
@@ -1,18 +1,41 @@
 CPU 8086
 SECTION data
-    str: db "Ciao mondo!$", 0
+    str: db "Hello$", 0
 
 SECTION text
 ..start:
     mov ax, data
     mov ds, ax
-    mov si, 0
+    mov si, str
+    xor di, str
+    xor cl, cl
+
 loop_string:
-    mov ah, [str+si]
-    cmp ah, 24h
-    je fine
-    inc si                  ; Expected value: 11 -> 0Bh
+    mov al, [si]
+    cmp al, 24h
+    je got_length
+    inc si
+    inc cl              ; String's length
     jmp loop_string
+
+got_length:
+    dec si
+
+move_pointer:
+    mov al, [si]     ; Tip: keep track of the string's memory address to calculate the endpoint
+    mov bl, [di]
+    mov [di], al
+    mov [si], bl          ; String's memory address         
+    cmp di, si
+    je print_string
+    dec si
+    inc di
+    jmp move_pointer
+
+print_string: 
+    mov ah, 09h
+    mov dx, str
+    int 21h
 fine:
     mov ah, 4ch
     int 21h
