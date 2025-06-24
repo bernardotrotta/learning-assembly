7c91f912ed5e4c16cf1261d91873fcc7e9b049e4
Bernardo Trotta <bernardotrotta@outlook.it>
2025-06-24 13:14:25 +0200

    Print a simple string using the '09h' function

--- test.asm
+++ test.asm
@@ -1,27 +1,16 @@
 CPU 8086
 SECTION data
-N: DB 255
+    str: db "Ciao mondo!$", 0
 
 SECTION text
 ..start:
-    MOV AX, data
-    MOV DS, AX
-    MOV AL, [N]
-    MOV CX, 03h
-convert:
-    MOV BL, 0Ah
-    XOR AH, AH
-    DIV BL
-    PUSH AX
-    LOOP convert
-    MOV CX, 03h
-print_bytes:
-    POP AX
-    ADD AH, '0'
-    MOV DL, AH
-    MOV AH, 02h
-    INT 21h
-    LOOP print_bytes
+    mov ax, data
+    mov ds, ax
+    
+    mov ah, 09h
+    mov dx, str
+    int 21h
+
 fine:
-    MOV AX, 4C00h
-    INT 21h
+    mov ah, 4ch
+    int 21h
