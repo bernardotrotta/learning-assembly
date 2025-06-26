1f9efd65d2308dae56d08cf343905d8b46a3b2dd
Bernardo Trotta <bernardotrotta@outlook.it>
2025-06-26 15:53:11 +0200

    Passing parameter to a function

--- test.asm
+++ test.asm
@@ -1,41 +1,26 @@
 CPU 8086
 SECTION data
-    str: db "Hello$", 0
+    char: db 'A'
+
 
 SECTION text
 ..start:
     mov ax, data
     mov ds, ax
-    mov si, str
-    xor di, str
-    xor cl, cl
-
-loop_string:
-    mov al, [si]
-    cmp al, 24h
-    je got_length
-    inc si
-    inc cl              ; String's length
-    jmp loop_string
-
-got_length:
-    dec si
-
-move_pointer:
-    mov al, [si]     ; Tip: keep track of the string's memory address to calculate the endpoint
-    mov bl, [di]
-    mov [di], al
-    mov [si], bl          ; String's memory address         
-    cmp di, si
-    je print_string
-    dec si
-    inc di
-    jmp move_pointer
-
-print_string: 
-    mov ah, 09h
-    mov dx, str
+    mov ax, char
+    push ax
+    call StampaChar
+    add sp, 2
+    mov ax, 4c00h
     int 21h
-fine:
-    mov ah, 4ch
+
+StampaChar: 
+    push bp
+    mov bp, sp
+    mov ah, 02h
+    mov bx, [bp+4]
+    mov dx, [bx]
     int 21h
+.fine:
+    pop bp
+    ret
