a058d572552cfc165399909f0cf7e595ad8aad4d
Bernardo Trotta <bernardotrotta@outlook.it>
2025-09-06 15:58:38 +0200

    Esercizio con stringa 1

--- test.asm
+++ test.asm
@@ -1,18 +1,25 @@
+; 4. Contatore semplice
+; Creare un loop che conta da 1 a 10 e visualizza ogni numero.
+
 CPU 8086
-
-SECTION data
-Frase: db 'Buongiorno', 0
-Messaggio: db 34, ': vocale (v/V) o conosnante (c/c)?'
-
-SECTION text
-..start:
+section data
+    string db 1
+section text
     mov ax, data
     mov ds, ax
 
-ScorriStringa:
+    mov cl, 0
 
-VerificaRisposta:
-
-Fine:
-    mov ax 4c00h
+    loop_start:
+    mov ah, 02h
+    mov dl, cl
+    add dl, '0'
     int 21h
+    inc cx
+    cmp cx, 0Ah ; 10
+    jne loop_start
+
+
+    mov ah, 4ch
+    int 21h
+
