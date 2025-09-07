393fb4ef8cedd95ae5c3f892b31ba16bae0cbe7c
Bernardo Trotta <bernardotrotta@outlook.it>
2025-09-07 17:43:20 +0200

    MOVSB

--- test.asm
+++ test.asm
@@ -1,35 +1,21 @@
-; Convertire tutti i caratteri di una stringa in maiuscolo
-; Poi creare la versione per minuscolo immagino si sommi 40
-cpu 8086
-section data
-str: db '!!CiaoMcFratm7$', 0
+; MOVSB – Move String Byte
+; Copia un byte da [DS:SI] a [ES:DI].
 
+section data
+    src: db 'Hello$', 0
+    dest: resb 20
 section text
 ..start:
     mov ax, data
     mov ds, ax
-    mov si, str
-controlla:
-    mov bl, [si]
-    ; controllo $
-    cmp bl, 24h
-    je stampa
-    ; controllo lettera a
-    cmp bl, 61h
-    jl avanti
-    ; controllo lettera z
-    cmp bl, 7ah
-    jg avanti
-converti:
-    sub bl, 20h
-    mov [si], bl
-avanti:
-    inc si
-    jne controlla
-stampa:
-    mov ah, 09h
-    mov dx, str
-    int 21h 
+
+    mov es, ax  ; Sposto nell'extra segment l'indirizzo di dati
+    mov si, src ; Sposto in SI l'indirizzo di inizio stringa
+    mov cx, 6   ; Sposto in cx il numero di caratteri da spostare H e l l o $
+
+    cld         ; Clear Direction Flag (SI e DI si decrementano)
+    rep movsb   ; Copia cx byte (in questo caso 6) da src a dests
+
 fine:
     mov ah, 04ch
     int 21h
