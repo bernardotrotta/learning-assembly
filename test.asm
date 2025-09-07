26324f4e9ec33367305b2d351ce0cd30185062d9
Bernardo Trotta <bernardotrotta@outlook.it>
2025-09-07 17:22:25 +0200

    Esercizio con strigna 2

--- test.asm
+++ test.asm
@@ -1,25 +1,35 @@
-; 4. Contatore semplice
-; Creare un loop che conta da 1 a 10 e visualizza ogni numero.
-
-CPU 8086
+; Convertire tutti i caratteri di una stringa in maiuscolo
+; Poi creare la versione per minuscolo immagino si sommi 40
+cpu 8086
 section data
-    string db 1
+str: db '!!CiaoMcFratm7$', 0
+
 section text
+..start:
     mov ax, data
     mov ds, ax
-
-    mov cl, 0
-
-    loop_start:
-    mov ah, 02h
-    mov dl, cl
-    add dl, '0'
+    mov si, str
+controlla:
+    mov bl, [si]
+    ; controllo $
+    cmp bl, 24h
+    je stampa
+    ; controllo lettera a
+    cmp bl, 61h
+    jl avanti
+    ; controllo lettera z
+    cmp bl, 7ah
+    jg avanti
+converti:
+    sub bl, 20h
+    mov [si], bl
+avanti:
+    inc si
+    jne controlla
+stampa:
+    mov ah, 09h
+    mov dx, str
+    int 21h 
+fine:
+    mov ah, 04ch
     int 21h
-    inc cx
-    cmp cx, 0Ah ; 10
-    jne loop_start
-
-
-    mov ah, 4ch
-    int 21h
-
