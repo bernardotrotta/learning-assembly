ac2b57fe2c38fba280f17184b4988ee22c6f0dcf
Bernardo Trotta <bernardotrotta@outlook.it>
2025-09-07 19:49:51 +0200

    SCASB

--- test.asm
+++ test.asm
@@ -1,19 +1,18 @@
-; LODSB – Load String Byte
-; Carica il byte in [DS:SI] dentro AL e aggiorna SI.
+; SCASB – Scan String Byte
+; Confronta AL con [ES:DI], aggiorna DI. Utile per cercare un carattere
 
 section data
-    str: db 'Bernardo$', 0
+    buffer: db 'ciao mondo$', 0
 section text
 ..start:
     mov ax, data
-    mov ds, ax
-    mov si, str
-    cld ; serve ad impostare la direzione dell'incremento degli indici DI e SI
-next_char:
-    lodsb
-    cmp al, 24h
-    je fine
-    jmp next_char
+    mov es, ax
+    mov di, buffer
+    mov al, 'o'
+    mov cx, 20
+
+    cld
+    repne scasb
 fine:
     mov ah, 04ch
     int 21h
