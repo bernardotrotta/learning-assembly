f3cd064f42cf911281cb4e8d06820a081f6bd3a1
Bernardo Trotta <bernardotrotta@outlook.it>
2025-09-10 12:35:22 +0200

    250212

--- test.asm
+++ test.asm
@@ -1,58 +1,66 @@
 section data   
-    Frase: db 'Ciao', 0
+    Frase: db 'Buonasera', 0
     N: dw 4
     Vett: db 2, 19, -1, 0
-
 section text
 ..start:
     mov ax, data
     mov ds, ax
     push Frase
-    mov ax, [N]             ; Stack lavora solo a 16 bit
+    mov ax, [N]
     push ax
     push Vett
     call OscuraCaratteri
-Fine:
-    MOV DX, Frase   ; DX punta all’inizio della stringa
-    MOV AH, 09h            ; funzione 09h di DOS
-    INT 21h                ; chiama DOS
-    mov ah, 04ch
+    mov si, 0
+    jmp StampaStringa
+StampaStringa:
+    mov dl, [si]
+    cmp dl, 0
+    je fine
+    mov ah, 02h
     int 21h
+    inc si
+    jmp StampaStringa
+
+
 OscuraCaratteri:
     push bp
     mov bp, sp
-    mov si, [bp + 8]        ; Indirizzo di frase    
-    xor bl, bl              ; Preparo il contatore per la lunghezza
-    xor dx, dx              ; Preparo il contatore per gli indici errati
-    cld
-LunghezzaStringa:
-    lodsb
-    cmp al, 0
-    je FineLunghezzaStringa
-    inc bl                  ; Lunghezza della stringa
-    jmp LunghezzaStringa
-FineLunghezzaStringa:
-    mov cx, [bp + 6]
-    mov si, [bp + 4]
-Verifica:
-    DEC cx
-    JZ FineStringa
-    lodsb
-    cmp al, bl              ; Verifico se l'indice è maggiore della stringa
-    jg IndiceErrato
-    cmp al, 0               ; Veridico se minore della stringa
-    jl IndiceErrato
-    jmp Sotituisci
 
-IndiceErrato:
-    inc dx
-    jmp Verifica
-Sotituisci:
-    push si
+    mov di, [bp + 4]   ; indirizzo vettore indici
+    mov cx, [bp + 6]   ; numero indici
+    mov si, [bp + 8]   ; indirizzo stringa
+    xor bl, bl
+LunghezzaStringa:
+    mov dl, [si]
+    cmp dl, 0
+    je VerificaIndici
+    inc si
+    inc bl              ; Lunghezza Stringa
+    jmp LunghezzaStringa    
+VerificaIndici:
+    mov dl, [di]
+    cmp dl, 0           ; È un indice più piccolo dello 0?
+    jl IndiceErrato
+    cmp dl, bl          ; È un indice più grande della lunghezza della riga?
+    jg IndiceErrato
     mov si, [bp + 8]
-    add si, ax
-    mov BYTE[si], 2ah           ; *
-    pop si
-    jmp Verifica
-FineStringa:
-    RET
+    xor dh, dh
+    add si, dx
+    mov BYTE[si], 2ah 
+    jmp ProssimoCarattere
+ProssimoCarattere:
+    inc di
+    dec cl
+    jnz VerificaIndici
+    mov sp, bp
+    pop bp
+    RET 6
+IndiceErrato:
+    inc al
+    jmp ProssimoCarattere 
+    
+
+fine:
+    mov ah, 04ch
+    int 21h
