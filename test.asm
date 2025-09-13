cf8b65cfb4390f6587af14f121437895b71cf238
Bernardo Trotta <bernardotrotta@outlook.it>
2025-09-13 19:10:57 +0200

    Scrittura su file

--- test.asm
+++ test.asm
@@ -1,79 +1,34 @@
 section data
-Vett: db 1, 22, 12, 7, 11
-N: db 5
-Err: db 0Dh, 0Ah, 'Non valido', 0Dh, 0Ah, '$'
+    Filename: db 'fprova', 0
+    Stringa: db 10, 'Buongiorno'
+    N: resb 1
+    handle: resw 1
+
 section text
 ..start:
     mov ax, data
     mov ds, ax
-    xor ax, ax
 
-    push Vett
-    mov ax, [N]
-    push ax
-    push Err
-
-LeggiCarattere:
-    mov ah, 01h
+    mov ah, 3ch
+    mov cx, 00h
+    mov dx, Filename
     int 21h
-    cmp al, 1bh
-    je fine
-    xor ah, ah
-    push ax 
-    call ControllaCarattere
-    jmp LeggiCarattere
-
-ControllaCarattere:
-    push bp
-    mov bp, sp
-    mov al, [bp + 4]
-    mov si, [bp + 10]
-    mov cx, [bp + 8]
-    xor ch, ch 
     
-    cmp al, 60h
-    jg ControllaMinuscola
-    cmp al, 40h
-    jg ControllaMaiuscola
-    jmp CarattereErrato
+    jc fine
+    mov [handle], ax
 
-ControllaMinuscola:
-    cmp al, 7ah
-    jg CarattereErrato
-    sub al, 60h
-    jmp ControllaVettore
-    jmp FineLoop
+    mov ah, 40h
+    mov dx, Stringa+1       ; Offset della stsringa
+    mov bx, [handle]        ; Filehandle
+    xor ch, ch              ; CX=0000
+    mov cl, [Stringa]       ; CX=000A
+    int 21h                     
+    mov [N], ax
 
-ControllaMaiuscola:
-    cmp al, 5ah
-    jg CarattereErrato
-    sub al, 40h
-    jmp ControllaVettore
-    jmp FineLoop
-
-CarattereErrato:
-    mov dx, [bp + 6]
-    mov ah, 09h
+    mov ah, 3eh
+    mov bx, [handle]
     int 21h
-
-ControllaVettore:
-    cmp al, [si]
-    je Incrementa
-    inc si
-    loop ControllaVettore
-    jmp Decrementa
-    jmp FineLoop
-
-Incrementa:
-    inc bx
-    jmp FineLoop
-Decrementa:
-    dec bx
-
-FineLoop:
-    pop bp
-    ret 2
 
 fine:
-    mov ah, 04ch
-    int 21h
+mov ax,4ch
+int 21h
